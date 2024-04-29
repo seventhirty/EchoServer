ed7c15ea0ff9f097804d2b41ca5afb45e372451b
seventhirty <seventhirtybg@gmail.com>
2024-04-29 13:09:43 +0300

    * minor inline methods readability improvement

--- lib/EchoServer/include/EchoServer/EchoServer.hpp
+++ lib/EchoServer/include/EchoServer/EchoServer.hpp
@@ -12,29 +12,27 @@ public:
   explicit EchoServer(std::unique_ptr<ISocketInterface> socketInterface);
 
   int Run(int port);
-  inline int GetActiveConnectionsCount();
+  int GetActiveConnectionsCount();
 
 private:
   EchoServer(const EchoServer &) = delete;
   EchoServer &operator=(const EchoServer &) = delete;
 
   int InitListeningSocket(int port) const;
-
   void AcceptNextClientConnection(int listenSocketFD);
   void HandleClientConnection(int clientConnectionSocketFD);
-
-  inline void UpdateActiveConnectionsCount(int delta);
+  void UpdateActiveConnectionsCount(int delta);
 
   NetworkService m_networkService;
   std::atomic_int m_activeConnectionsCount;
 };
 
-int EchoServer::GetActiveConnectionsCount()
+inline int EchoServer::GetActiveConnectionsCount()
 {
   return m_activeConnectionsCount;
 }
 
-void EchoServer::UpdateActiveConnectionsCount(int delta)
+inline void EchoServer::UpdateActiveConnectionsCount(int delta)
 {
   m_activeConnectionsCount += delta;
 }
--- lib/NetworkService/include/NetworkService/SocketInterface.hpp
+++ lib/NetworkService/include/NetworkService/SocketInterface.hpp
@@ -5,53 +5,53 @@
 class SocketInterface : public ISocketInterface
 {
 public:
-  inline int CreateSocket(int domain, int type, int protocol) const override;
-  inline int Bind(int socketFD, const sockaddr *socketAddress, socklen_t addrlen) const override;
-  inline int Listen(int socketFD, int maxPendingConnectionsCount) const override;
-  inline int Accept(int socketFD, sockaddr *out_socketAddress, socklen_t *out_addrLen) const override;
-  inline int Close(int socketFD) const override;
-  inline ssize_t Read(int socketFD, void *buffer, size_t nBytes) const override;
-  inline ssize_t Write(int socketFD, const void *buffer, size_t nBytes) const override;
-  inline ssize_t SetOption(int socketFD, int protocolLevel,
+  int CreateSocket(int domain, int type, int protocol) const override;
+  int Bind(int socketFD, const sockaddr *socketAddress, socklen_t addrlen) const override;
+  int Listen(int socketFD, int maxPendingConnectionsCount) const override;
+  int Accept(int socketFD, sockaddr *out_socketAddress, socklen_t *out_addrLen) const override;
+  int Close(int socketFD) const override;
+  ssize_t Read(int socketFD, void *buffer, size_t nBytes) const override;
+  ssize_t Write(int socketFD, const void *buffer, size_t nBytes) const override;
+  ssize_t SetOption(int socketFD, int protocolLevel,
                            int optionName, const void *optionValue, socklen_t optionLen) const override;
 };
 
-int SocketInterface::CreateSocket(int domain, int type, int protocol) const
+inline int SocketInterface::CreateSocket(int domain, int type, int protocol) const
 {
   return socket(domain, type, protocol);
 }
 
-int SocketInterface::Bind(int socketFD, const sockaddr *socketAddress, socklen_t addrlen) const
+inline int SocketInterface::Bind(int socketFD, const sockaddr *socketAddress, socklen_t addrlen) const
 {
   return bind(socketFD, socketAddress, addrlen);
 }
 
-int SocketInterface::Listen(int socketFD, int maxPendingConnectionsCount) const
+inline int SocketInterface::Listen(int socketFD, int maxPendingConnectionsCount) const
 {
   return listen(socketFD, maxPendingConnectionsCount);
 }
 
-int SocketInterface::Accept(int socketFD, sockaddr *out_socketAddress, socklen_t *out_addrLen) const
+inline int SocketInterface::Accept(int socketFD, sockaddr *out_socketAddress, socklen_t *out_addrLen) const
 {
   return accept(socketFD, out_socketAddress, out_addrLen);
 }
 
-int SocketInterface::Close(int socketFD) const
+inline int SocketInterface::Close(int socketFD) const
 {
   return close(socketFD);
 }
 
-ssize_t SocketInterface::Read(int socketFD, void *buffer, size_t nBytes) const
+inline ssize_t SocketInterface::Read(int socketFD, void *buffer, size_t nBytes) const
 {
   return read(socketFD, buffer, nBytes);
 }
 
-ssize_t SocketInterface::Write(int socketFD, const void *buffer, size_t nBytes) const
+inline ssize_t SocketInterface::Write(int socketFD, const void *buffer, size_t nBytes) const
 {
   return write(socketFD, buffer, nBytes);
 }
 
-ssize_t SocketInterface::SetOption(int socketFD, int protocolLevel, int optionName, 
+inline ssize_t SocketInterface::SetOption(int socketFD, int protocolLevel, int optionName, 
                                    const void *optionValue, socklen_t optionLen) const
 {
   return setsockopt(socketFD, protocolLevel, optionName, optionValue, optionLen);
